8ed6576d26087c67ecee40fd9f32d25ef630a61a
Rylan Greer <rylangreer@Rylans-MacBook-Pro.local>
2025-11-02 15:01:19 +0000

    add in filepath if wanted@

--- main.cpp
+++ main.cpp
@@ -3,9 +3,12 @@
 #include "src/grid_reader/grid_reader.hpp"
 #include "src/solver_wrapper/solver_wrapper.hpp"
 
-int main()
+int main(int argc, char** argv)
 {
     const char* filename = "grids.txt";
+    if(argc > 1)
+        filename = argv[1];
+    
     int solved_count = Sudoku::solveGridsFromFile(filename, true);
     return 0;
 }
